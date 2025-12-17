3a791e2e38426b5e396c836dc09c447800734f9b
Toni Ahn <ntoniahn@gmail.com>
2025-12-17 22:13:38 +0800

    Fix build error: remove lingering usage of isSignUp

--- my-app/src/components/login/Login.jsx
+++ my-app/src/components/login/Login.jsx
@@ -1301,7 +1301,7 @@ const Login = () => {
                       <div className="w-5 h-5 border-2 border-[#7A1C1C] border-t-transparent rounded-full animate-spin"></div>
                     ) : (
                       <>
-                        {isSignUp ? "Sign Up" : "Sign in"}
+                        Sign in
                         <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                           <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
                         </svg>
